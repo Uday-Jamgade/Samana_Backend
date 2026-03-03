0c40212a4537e6c986cc50c04c3c75ffe6447854
Uday Jamgade <udayjamgade1@gmail.com>
2026-03-03 18:49:51 +0530

    Fix: Remove deprecated MongoDB connection options

--- DB/Config.js
+++ DB/Config.js
@@ -3,10 +3,8 @@ require('dotenv').config();
 const url = process.env.URL;
 
 mongoose.connect(url,{
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
   tlsAllowInvalidCertificates: false,
- 
+  tlsInsecure: false
 })
   .then(() => {
     console.log("Connected to MongoDB successfully");   
