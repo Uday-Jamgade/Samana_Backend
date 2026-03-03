4a3949f5fdea24f34cd0c3d0a0fca17d3ab23755
Uday Jamgade <udayjamgade1@gmail.com>
2026-03-03 19:06:10 +0530

    Fix: Add proper TLS configuration for MongoDB connection

--- DB/Config.js
+++ DB/Config.js
@@ -2,7 +2,11 @@ const mongoose = require("mongoose");
 require('dotenv').config();
 const url = process.env.URL;
 
-mongoose.connect(url)
+mongoose.connect(url, {
+  tls: true,
+  retryWrites: true,
+  serverSelectionTimeoutMS: 5000,
+})
   .then(() => {
     console.log("Connected to MongoDB successfully");   
     })
